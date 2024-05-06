087d965eb12532a040c004ef41353ca13e7fcb5f
JoelS711 <81266779+JoelS711@users.noreply.github.com>
2024-05-06 17:51:22 -0500

    Se modifica el ts de sales

--- src/app/components/sales/sales.component.ts
+++ src/app/components/sales/sales.component.ts
@@ -10,9 +10,8 @@ export class SalesComponent implements OnInit {
 
   ngOnInit(): void {
     this.getConsecutivo();
-    this.clientedata;
-    /*this.addLine();*/
-
+    this.clientedata = { "nameClient": "" };
+    
   }
 
   constructor(private clientehttp: HttpClient) { }
@@ -34,56 +33,60 @@ export class SalesComponent implements OnInit {
 
 
 
-  cod:number=0;
-  nameProduct:string="";
-  price:number=0;
-  quantity:number=0;
-  lastIndex: number=0;
-  addProducts: any[] = [];
-  addLine(){
-    this.addProducts.push({
-      cod: this.cod,
-      name: this.nameProduct,
-      quantity: this.quantity,
-      price: this.price // Puedes inicializar el precio como desees
-    });
-  }
+
 
   
-
-  removeLine(){
-    if (this.addProducts.length > 1) {
-      this.addProducts.pop();
-      this.products.pop();
-
-    }
-  }
 
   cedulacliente!: any;
   clientedata: any;
   getCliente() {
     this.clientehttp.get(this.apiURLClient + "clients/identification/" + this.cedulacliente).subscribe((data) => {
       this.clientedata = data;
-      console.log(this.clientedata)
     }
     )
   }
 
 
-  products: any[] = [];
-  cods: any[] = [];
-  quant:any[]=[];
-  getProduct(numproducto: number) {
-    const cod = this.cods[numproducto - 1];
+  productsData: any []= [];
+  productName: string = "";
+  cods: number = 0;
+  quant: number = 0;
+  price:number=0;
+  priceT:number=0;
+  getProduct() {
+    const cod = this.cods;
     if (cod) {
       this.clientehttp.get(this.apiURLProduct + "products/code/" + cod)
-        .subscribe((data) => {
-          this.products[numproducto - 1] = data;
-          console.log(this.products);
+        .subscribe((data: any) => {
+          this.productsData = data;
+          this.productName = this.productsData[0].name
+          console.log(this.quant);
+          console.log(this.productsData);
         });
     }
+
   }
- reload() {
+
+  calcPrice() {
+    if (this.quant && this.productsData && this.productsData.length > 0) {
+      this.price = this.productsData[0].saleprice * this.quant;
+    } else {
+      this.price = 0; // O cualquier valor predeterminado si quantity no está definido
+    }
+  }
+
+  addProducts: any []=[];
+  addLine(){
+    this.addProducts.push({
+      code: this.cods,
+      name: this.productName,
+      quantity: this.quant,
+      price:this.price
+    })
+    console.log(this.addProducts)
+  }
+
+  reload() {
     window.location.reload()
   }
 
@@ -91,205 +94,205 @@ export class SalesComponent implements OnInit {
     switch (numproducto) {
       case 1:
         break;
-      }
+    }
   }
 
-  postVenta(){}
+  postVenta() { }
 
-  ciudad:any;
-  precioprod:any;
-  totalventa:any;
-  totaliva:any;
-  totalplusiva:any;
-
-
-
-
-
-
-
-  }
-
-
-
-
-  /*product1: any;
-  product2: any;
-  product3: any;
-  codprod1: any;
-  codprod2: any;
-  codprod3: any;
-  getProducto(numproducto: number) {
-    switch (numproducto) {
-      case 1:
-        this.clientehttp.get(this.apiURLProduct + "products/code/" + this.codprod1)
-          .subscribe((data) => {
-            this.product1 = data;
-          });
-        break;
-      case 2:
-        this.clientehttp.get(this.apiURLProduct + "products/code/" + this.codprod2)
-          .subscribe((data) => {
-            this.product2 = data;
-          });
-        break;
-      case 3:
-        this.clientehttp.get(this.apiURLProduct + "products/code/" + this.codprod3)
-          .subscribe((data) => {
-            this.product3 = data;
-          });
-        break;
-
-      default:
-        break;
-    }
-  }*/
-
- /* precioprod1: any;
-  cant1: any;
-  precioprod2: any;
-  cant2: any;
-  precioprod3: any;
-  cant3: any;
-  calcPrecioProd(numproducto: number) {
-    switch (numproducto) {
-      case 1:
-        this.precioprod1 = this.cant1 * this.product1[0].saleprice;
-        break;
-      case 2:
-        this.precioprod2 = this.cant2 * this.product2[0].saleprice;
-        break;
-      case 3:
-        this.precioprod3 = this.cant3 * this.product3[0].saleprice;
-        break;
-
-      default:
-        break;
-    };
-    this.calcularTotales();
-  }
-  totalventa: number = 0.0;
-  totalplusiva: number = 0.0;
-  totaliva: number = 0.0;
-  calcularTotales() {
-    this.totalventa = 0.0;
-    if (this.precioprod1 != null && this.precioprod1 != undefined
-      && this.precioprod1 != "") {
-      this.totalventa += this.precioprod1;
-      this.totaliva = (this.totalventa) * 0.19;
-      this.totalplusiva = this.totalventa + ((this.totalventa) * 0.19);
-
-    }
-    if (this.precioprod2 != null && this.precioprod2 != undefined
-      && this.precioprod2 != "") {
-      this.totalventa += this.precioprod2;
-      this.totaliva = (this.totalventa) * 0.19;
-      this.totalplusiva = this.totalventa + ((this.totalventa) * 0.19);
-
-    }
-    if (this.precioprod3 != null && this.precioprod3 != undefined
-      && this.precioprod3 != "") {
-      this.totalventa += this.precioprod3;
-      this.totaliva = (this.totalventa) * 0.19;
-      this.totalplusiva = this.totalventa + ((this.totalventa) * 0.19);
-
-    }
-
-  }
-
-  codigorespuesta: any;
-  listadetalleventa = Array();
   ciudad: any;
-  postVenta() {
-    if (this.precioprod1 != null && this.precioprod1 != undefined && this.precioprod1 != "") {
-      let aux = {
-        "productquantity": this.cant1,
-        "codeproduct": this.codprod1,
-        "ivavalue": this.precioprod1 * 0.19,
-        "totalsale": this.precioprod1,
-        "salevalue": (this.precioprod1 * 0.19) + this.precioprod1
-      }
-      this.listadetalleventa.push(aux);
-
-    }
-    if (this.precioprod2 != null && this.precioprod2 != undefined && this.precioprod2 != "") {
-      let aux = {
-        "productquantity": this.cant2,
-        "codeproduct": this.codprod2,
-        "ivavalue": this.precioprod2 * 0.19,
-        "totalsale": this.precioprod2,
-        "salevalue": (this.precioprod2 * 0.19) + this.precioprod2
-      }
-      this.listadetalleventa.push(aux);
-
-    }
-    if (this.precioprod3 != null && this.precioprod3 != undefined && this.precioprod3 != "") {
-      let aux = {
-        "productquantity": this.cant3,
-        "codeproduct": this.codprod3,
-        "ivavalue": this.precioprod3 * 0.19,
-        "totalsale": this.precioprod3,
-        "salevalue": (this.precioprod3 * 0.19) + this.precioprod3
-      }
-      this.listadetalleventa.push(aux);
-
-    }
-    this.clientehttp.post(this.apiURL + "sales",
-      {
-        "identification": this.cedulacliente,
-        "salecode": this.consecutivo,
-        "saledetail": this.listadetalleventa,
-        "ivasale": this.totaliva,
-        "totalsale": this.totalplusiva,
-        "salevalue": this.totalventa
-      }, {
-      observe: 'response'
-    }).subscribe(
-      (response: any) => {
-
-        this.codigorespuesta = response.status;
-
-        switch (this.codigorespuesta) {
-          case 0:
-            console.log(this.codigorespuesta);
-            /*this.showNotification('top', 'right', 1);*/
-         /*   break;
-          case 201:
-            this.postConsolidado();
-            /*this.showNotification('top', 'right', 1);*/
-            /*break;
-
-          case 226:
-            console.log(this.codigorespuesta);
-           /* this.showNotification('top', 'right', 2);*/
-            /*break;
-
-          case 500:
-            console.log(this.codigorespuesta);
-           /* this.showNotification('top', 'right', 3);*/
-            /*break;
-
-        }
-
-      }
-    );
+  precioprod: any;
+  totalventa: any;
+  totaliva: any;
+  totalplusiva: any;
 
 
+
+
+
+
+
+}
+
+
+
+
+/*product1: any;
+product2: any;
+product3: any;
+codprod1: any;
+codprod2: any;
+codprod3: any;
+getProducto(numproducto: number) {
+  switch (numproducto) {
+    case 1:
+      this.clientehttp.get(this.apiURLProduct + "products/code/" + this.codprod1)
+        .subscribe((data) => {
+          this.product1 = data;
+        });
+      break;
+    case 2:
+      this.clientehttp.get(this.apiURLProduct + "products/code/" + this.codprod2)
+        .subscribe((data) => {
+          this.product2 = data;
+        });
+      break;
+    case 3:
+      this.clientehttp.get(this.apiURLProduct + "products/code/" + this.codprod3)
+        .subscribe((data) => {
+          this.product3 = data;
+        });
+      break;
+
+    default:
+      break;
   }
- /* postConsolidado() {
-    console.log(this.ciudad)
-    console.log(typeof this.ciudad)
-    this.clientehttp.post(this.apiURLConsolidated + "consolidated/agregar/"+this.ciudad, 
-    {},
-    {observe:"response"}
-    ).subscribe((response: any) => {
+}*/
 
-      console.log(response.status)
+/* precioprod1: any;
+ cant1: any;
+ precioprod2: any;
+ cant2: any;
+ precioprod3: any;
+ cant3: any;
+ calcPrecioProd(numproducto: number) {
+   switch (numproducto) {
+     case 1:
+       this.precioprod1 = this.cant1 * this.product1[0].saleprice;
+       break;
+     case 2:
+       this.precioprod2 = this.cant2 * this.product2[0].saleprice;
+       break;
+     case 3:
+       this.precioprod3 = this.cant3 * this.product3[0].saleprice;
+       break;
 
-    });
-  }*/
+     default:
+       break;
+   };
+   this.calcularTotales();
+ }
+ totalventa: number = 0.0;
+ totalplusiva: number = 0.0;
+ totaliva: number = 0.0;
+ calcularTotales() {
+   this.totalventa = 0.0;
+   if (this.precioprod1 != null && this.precioprod1 != undefined
+     && this.precioprod1 != "") {
+     this.totalventa += this.precioprod1;
+     this.totaliva = (this.totalventa) * 0.19;
+     this.totalplusiva = this.totalventa + ((this.totalventa) * 0.19);
 
- 
+   }
+   if (this.precioprod2 != null && this.precioprod2 != undefined
+     && this.precioprod2 != "") {
+     this.totalventa += this.precioprod2;
+     this.totaliva = (this.totalventa) * 0.19;
+     this.totalplusiva = this.totalventa + ((this.totalventa) * 0.19);
+
+   }
+   if (this.precioprod3 != null && this.precioprod3 != undefined
+     && this.precioprod3 != "") {
+     this.totalventa += this.precioprod3;
+     this.totaliva = (this.totalventa) * 0.19;
+     this.totalplusiva = this.totalventa + ((this.totalventa) * 0.19);
+
+   }
+
+ }
+
+ codigorespuesta: any;
+ listadetalleventa = Array();
+ ciudad: any;
+ postVenta() {
+   if (this.precioprod1 != null && this.precioprod1 != undefined && this.precioprod1 != "") {
+     let aux = {
+       "productquantity": this.cant1,
+       "codeproduct": this.codprod1,
+       "ivavalue": this.precioprod1 * 0.19,
+       "totalsale": this.precioprod1,
+       "salevalue": (this.precioprod1 * 0.19) + this.precioprod1
+     }
+     this.listadetalleventa.push(aux);
+
+   }
+   if (this.precioprod2 != null && this.precioprod2 != undefined && this.precioprod2 != "") {
+     let aux = {
+       "productquantity": this.cant2,
+       "codeproduct": this.codprod2,
+       "ivavalue": this.precioprod2 * 0.19,
+       "totalsale": this.precioprod2,
+       "salevalue": (this.precioprod2 * 0.19) + this.precioprod2
+     }
+     this.listadetalleventa.push(aux);
+
+   }
+   if (this.precioprod3 != null && this.precioprod3 != undefined && this.precioprod3 != "") {
+     let aux = {
+       "productquantity": this.cant3,
+       "codeproduct": this.codprod3,
+       "ivavalue": this.precioprod3 * 0.19,
+       "totalsale": this.precioprod3,
+       "salevalue": (this.precioprod3 * 0.19) + this.precioprod3
+     }
+     this.listadetalleventa.push(aux);
+
+   }
+   this.clientehttp.post(this.apiURL + "sales",
+     {
+       "identification": this.cedulacliente,
+       "salecode": this.consecutivo,
+       "saledetail": this.listadetalleventa,
+       "ivasale": this.totaliva,
+       "totalsale": this.totalplusiva,
+       "salevalue": this.totalventa
+     }, {
+     observe: 'response'
+   }).subscribe(
+     (response: any) => {
+
+       this.codigorespuesta = response.status;
+
+       switch (this.codigorespuesta) {
+         case 0:
+           console.log(this.codigorespuesta);
+           /*this.showNotification('top', 'right', 1);*/
+/*   break;
+ case 201:
+   this.postConsolidado();
+   /*this.showNotification('top', 'right', 1);*/
+/*break;
+
+case 226:
+console.log(this.codigorespuesta);
+/* this.showNotification('top', 'right', 2);*/
+/*break;
+
+case 500:
+console.log(this.codigorespuesta);
+/* this.showNotification('top', 'right', 3);*/
+/*break;
+
+}
+
+}
+);
+
+
+}
+/* postConsolidado() {
+console.log(this.ciudad)
+console.log(typeof this.ciudad)
+this.clientehttp.post(this.apiURLConsolidated + "consolidated/agregar/"+this.ciudad, 
+{},
+{observe:"response"}
+).subscribe((response: any) => {
+
+console.log(response.status)
+
+});
+}*/
+
+
 
 
 
