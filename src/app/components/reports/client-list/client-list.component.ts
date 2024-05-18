1be9bb60770541eb454a9f79d4a629ec21571f37
JoelS711 <81266779+JoelS711@users.noreply.github.com>
2024-05-17 21:41:05 -0500

    Se carga URL de las rutas del back en el TS de reports

--- src/app/components/reports/client-list/client-list.component.ts
+++ src/app/components/reports/client-list/client-list.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './client-list.component.css'
 })
 export class ClientListComponent {
+
+  constructor(private clientehttp: HttpClient) { }
+
+  apiURL: string = "http://localhost:8082/api/";
+  apiURLClient: string = "http://localhost:8081/api/";
+  apiURLProduct: string = "http://localhost:8080/api/";
+  apiURLConsolidated: string = "http://localhost:8085/api/";
+
 
 }
--- src/app/components/reports/sales-city/sales-city.component.ts
+++ src/app/components/reports/sales-city/sales-city.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './sales-city.component.css'
 })
 export class SalesCityComponent {
+  constructor(private clientehttp: HttpClient) { }
 
+  apiURL: string = "http://localhost:8082/api/";
+  apiURLClient: string = "http://localhost:8081/api/";
+  apiURLProduct: string = "http://localhost:8080/api/";
+  apiURLConsolidated: string = "http://localhost:8085/api/";
 }
